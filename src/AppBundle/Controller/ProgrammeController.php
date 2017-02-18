e02c3e559d7de5513500774797e614826c1c9962
Antoine Gourtay <antoine.gourtay@hetic.net>
2017-02-18 19:23:50 +0100

    Adding Programme controller

---
+++ src/AppBundle/Controller/ProgrammeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ProgrammeController extends Controller
+{
+    /**
+     * @Route("/programme")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $programmes = $em->getRepository('AppBundle:Programme')->findAll();
+        $actualities = $em->getRepository('AppBundle:Actuality')->findAll();
+
+        return $this->render('AppBundle:Programme:index.html.twig', array(
+            'actualities' => $actualities,
+            'programmes' => $programmes,
+        ));
+    }
+
+}
